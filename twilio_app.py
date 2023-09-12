1b34e30c65134199152ee5da9bd257c9c5bb8f8b
hwang <readercn@gmail.com>
2023-09-12 16:48:01 +0700

    load sc price info

--- twilio_app.py
+++ twilio_app.py
@@ -3,6 +3,7 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
+from llama_index import download_loader
 
 st.set_page_config(page_title="Chat with the Twilio SMS docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
@@ -22,8 +23,17 @@ def load_data():
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
+    
+def load_twilio_data():
+    SimpleWebPageReader = download_loader("SimpleWebPageReader")
 
-index = load_data()
+    loader = SimpleWebPageReader()
+    documents = loader.load_data(urls=['https://support.twilio.com/hc/en-us/articles/226460288-How-much-does-a-Short-Code-cost-'])
+    service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+    index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+    return index
+
+index = load_twilio_data()
 # chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
